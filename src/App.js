74170048b48989d2d7932d2c6ea037834fb91295
Nick Ochieng <1nickochieng@gmail.com>
2023-08-16 14:01:21 +0300

    food site

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import HomePage from './component/HomePage';
+import Navbar from './component/Navbar';
+import FastFood from './component/fastfood';
+import Menu from './component/Menu';
+import Footer from './component/Footer';
+
+function App() {
+  
+  return(
+    <div>
+<Navbar />
+<HomePage />
+<FastFood />
+<Menu />
+<Footer />
+    </div>
+  )
+}
+
+export default App;
